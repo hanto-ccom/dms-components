d441d752d4ce38de2775cc88fa1d511280c3375d
Hannes <hannes.tollborg@centercom.se>
2025-03-19 14:17:26 +0100

    click event

--- projects/dms/src/lib/dms-button/dms-button.component.ts
+++ projects/dms/src/lib/dms-button/dms-button.component.ts
@@ -1,6 +1,8 @@
 import {
   Component,
+  EventEmitter,
   Input,
+  Output,
 } from '@angular/core';
 
 @Component({
@@ -13,4 +15,12 @@ import {
 })
 export class DmsButtonComponent {
   @Input() label: string = 'Click me';
+  @Output() clickEvent = new EventEmitter<void>();
+
+
+  handleClick() {
+    console.log("Button clicked! ")
+    //emit event to parent if needed
+    this.clickEvent.emit();
+  }
 }
